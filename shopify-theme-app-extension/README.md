d39bb5e12827fb21d0c137b81274c00f8e0dcc42
lemanh-tuong <letuong05092000@gmail.com>
2022-06-24 11:50:07 +0700

    them README

---
+++ shopify-theme-app-extension/README.md
@@ -0,0 +1,1 @@
+# [Tài liệu liên quan](https://wilokesoftware.atlassian.net/wiki/spaces/SP/pages/382402561/Theme+app+extension)
--- shopify-theme-app-extension/assets/APP_NAME.js
+++ shopify-theme-app-extension/assets/APP_NAME.js
@@ -1,17 +1,28 @@
 console.log("APP_NAME's initing");
 
 const LOAD_SCRIPT = () => {
-  console.log('APP_NAME loaded');
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'APP_NAME.netlify.js';
-  document.body.appendChild(script);
-  script.addEventListener('load', () => {
-    console.log('APP_NAME loaded');
-  });
-  script.addEventListener('error', e => {
-    console.log('APP_NAME load failed', e);
-  });
+	console.log("APP_NAME loaded");
+	const script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "APP_NAME.netlify.js";
+	document.body.appendChild(script);
+	script.addEventListener("load", () => {
+		console.log("APP_NAME loaded");
+	});
+	script.addEventListener("error", (e) => {
+		console.log("APP_NAME load failed", e);
+	});
 };
 
 LOAD_SCRIPT();
+
+// gán tên theme lên body để hỗ trợ việc custom css
+const interval = setInterval(() => {
+	try {
+		const bodyThemeName = `magic-badges-${window.Shopify.theme.name.toLowerCase().replaceAll(" ", "-")}`;
+		document.body.classList.add(bodyThemeName);
+		if (document.body.className.includes(bodyThemeName)) {
+			clearInterval(interval);
+		}
+	} catch {}
+}, 500);
