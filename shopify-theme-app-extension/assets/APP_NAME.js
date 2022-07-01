0dc3aeb634f89135fa0f83beee26674143446bd3
lemanh-tuong <letuong05092000@gmail.com>
2022-07-01 11:36:02 +0700

    update shopify-theme-app-extension boilerplate

--- shopify-theme-app-extension/assets/APP_NAME.js
+++ shopify-theme-app-extension/assets/APP_NAME.js
@@ -2,16 +2,16 @@ console.log("APP_NAME's initing");
 
 const LOAD_SCRIPT = () => {
 	console.log("APP_NAME loaded");
-	const script = document.createElement("script");
-	script.type = "text/javascript";
-	script.src = "APP_NAME.netlify.js";
-	document.body.appendChild(script);
-	script.addEventListener("load", () => {
-		console.log("APP_NAME loaded");
+	const $scriptEl = document.createElement("script");
+	$scriptEl.type = "text/javascript";
+	$scriptEl.src = "APP_NAME.netlify.js";
+	$scriptEl.addEventListener("load", () => {
+		console.log("APP_NAME - JS loaded");
 	});
-	script.addEventListener("error", (e) => {
-		console.log("APP_NAME load failed", e);
+	$scriptEl.addEventListener("error", (e) => {
+		console.log("APP_NAME - JS load failed", e);
 	});
+	document.body.appendChild($scriptEl);
 };
 
 LOAD_SCRIPT();
@@ -19,7 +19,7 @@ LOAD_SCRIPT();
 // gán tên theme lên body để hỗ trợ việc custom css
 const interval = setInterval(() => {
 	try {
-		const bodyThemeName = `magic-badges-${window.Shopify.theme.name.toLowerCase().replaceAll(" ", "-")}`;
+		const bodyThemeName = `APP_NAME-${window.Shopify.theme.name.toLowerCase().replaceAll(" ", "-")}`;
 		document.body.classList.add(bodyThemeName);
 		if (document.body.className.includes(bodyThemeName)) {
 			clearInterval(interval);
